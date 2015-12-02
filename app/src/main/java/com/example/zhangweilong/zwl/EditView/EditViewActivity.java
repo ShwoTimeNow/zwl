801ae47e4dd00ce063dce0f8c5ba30929149fcb0
zhangweilong <437838009@qq.com>
2015-12-02 17:54:27 +0800

    自定义viewgroup，实现viewpager效果

---
+++ app/src/main/java/com/example/zhangweilong/zwl/EditView/EditViewActivity.java
@@ -0,0 +1,14 @@
+package com.example.zhangweilong.zwl.EditView;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by zhangweilong on 15/12/2.
+ */
+public class EditViewActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
---
+++ app/src/main/java/com/example/zhangweilong/zwl/TextView/TextViewActivity.java
@@ -0,0 +1,14 @@
+package com.example.zhangweilong.zwl.TextView;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by zhangweilong on 15/12/2.
+ */
+public class TextViewActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
